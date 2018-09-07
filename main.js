674dc2044ee482ed7e01816e1503d3e119f9b52a
LudovicLachance <ludovic.lachance@gmail.com>
2018-09-06 22:08:58 -0400

    Add cmd to join git

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 require('dotenv').config()
-let Discord = require('discord.js');
+const Discord = require('discord.js');
+const octokit = require('@octokit/rest')({
+  timeout: 0, // 0 means no request timeout
+  headers: {
+    accept: 'application/vnd.github.v3+json',
+    'user-agent': 'octokit/rest.js v1.2.3' // v1.2.3 will be current version
+  }, 
+  // custom GitHub Enterprise URL
+  baseUrl: 'https://api.github.com', 
+  // Node only: advanced request options can be passed as http(s) agent
+  agent: undefined
+});
+
+octokit.authenticate({
+  type: 'basic',
+  username: process.env.GITHUB_USERNAME,
+  password: process.env.GITHUB_PASS,
+})
 
 let botModule = (function(client) {
   client.on('ready', () => {
@@ -10,6 +27,7 @@ let botModule = (function(client) {
 
   client.on('message', msg => {
     let cmd = msg.content;
+    cmd = cmd.split(' ')[0];
     cmd = cmd.replace('!', '');
     cmd = cmd.charAt(0).toUpperCase() + cmd.slice(1);
     cmd = 'on' + cmd;
@@ -51,6 +69,15 @@ botModule.addCmd('onRoll', function(msg) {
     + ' has rolled '
     + (Math.floor(Math.random() * 100) + 1)
     + '!'
+  );
+});
+
+botModule.addCmd('onGitinvite', function(msg) {
+  octokit.repos.addCollaborator({
+    owner: process.env.GITHUB_USERNAME,
+    repo: process.env.GITHUB_REPO,
+    username: msg.content.split(' ')[1]},
+    (error, result) => {}
   );
 });
 
