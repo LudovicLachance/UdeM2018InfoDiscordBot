e6acaf0b3a8710861edf186f5d04bef75ecf0c43
Ludovic Lachance <ludovic.lachance@gmail.com>
2018-09-06 20:25:08 -0400

    Change code for module pattern

--- main.js
+++ main.js
@@ -1,43 +1,57 @@
 require('dotenv').config()
-const Discord = require('discord.js');
-const client = new Discord.Client();
+let Discord = require('discord.js');
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+let botModule = (function(client) {
+  client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
 
-client.on('message', msg => {
-  let actions = {
-    '!roll': onRoll,
-    '!ping': onPing,
-  };
+  let commands = {};
 
-  let cmd = msg.content;
+  client.on('message', msg => {
+    let cmd = msg.content;
+    cmd = cmd.replace('!', '');
+    cmd = cmd.charAt(0).toUpperCase() + cmd.slice(1);
+    cmd = 'on' + cmd;
 
-  if (cmd in actions) {
-    actions[cmd](msg);
+    if (cmd in commands) {
+      commands[cmd](msg);
+    }
+  });
+
+  client.on('messageReactionAdd', (reaction, user) => {
+    reaction.message.channel.send(user.username
+      + ' reacted to '
+      + reaction.message.author
+      + ' with '
+      + reaction._emoji.name);
+  });
+
+  function addCmd(name, funct) {
+    commands[name] = funct;
   }
+
+  function start() {
+    client.login(process.env.BOT_KEY);
+  }
+
+  return {
+    start,
+    addCmd,
+  };
+})(new Discord.Client());
+
+botModule.addCmd('onPing', function(msg) {
+  msg.channel.send('pong');
 });
 
-function onPing(msg) {
-  msg.channel.send('pong');
-}
-
-function onRoll(msg) {
+botModule.addCmd('onRoll', function(msg) {
   msg.channel.send(
     msg.author.username
     + ' has rolled '
     + (Math.floor(Math.random() * 100) + 1)
     + '!'
   );
-}
-
-client.on('messageReactionAdd', (reaction, user) => {
-  reaction.message.channel.send(user.username
-    + ' reacted to '
-    + reaction.message.author
-    + ' with '
-    + reaction._emoji.name);
 });
 
-client.login(process.env.BOT_KEY);
+botModule.start();
